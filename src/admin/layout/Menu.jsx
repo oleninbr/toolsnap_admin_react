2a4b82d21707b3776797f3a71bfab5eb8bc4f36f
Bohdan Olenin <bohdan.olenin@oa.edu.ua>
2026-02-22 01:25:24 +0200

    fixed

--- src/admin/layout/Menu.jsx
+++ src/admin/layout/Menu.jsx
@@ -1,4 +1,5 @@
-import { Menu } from "react-admin";
+import { Menu, useSidebarState } from "react-admin";
+import { Box, Divider, Typography } from "@mui/material";
 import BuildRoundedIcon from "@mui/icons-material/BuildRounded";
 import CategoryRoundedIcon from "@mui/icons-material/CategoryRounded";
 import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
@@ -9,23 +10,74 @@ import PrecisionManufacturingRoundedIcon from "@mui/icons-material/PrecisionManu
 import ApartmentRoundedIcon from "@mui/icons-material/ApartmentRounded";
 import MapRoundedIcon from "@mui/icons-material/MapRounded";
 
-export const CustomMenu = () => (
-  <Menu>
+export const CustomMenu = () => {
+  const [open] = useSidebarState();
+
+  return (
+    <Menu>
+      {open && (
+        <Box sx={{ px: 2, pt: 2, pb: 1 }}>
+          <Typography variant="overline" sx={{ fontWeight: 700, letterSpacing: 1 }}>
+            Tools
+          </Typography>
+        </Box>
+      )}
     <Menu.Item to="/tools" primaryText="Tools" leftIcon={<BuildRoundedIcon />} />
     <Menu.Item to="/tool-types" primaryText="Tool Types" leftIcon={<CategoryRoundedIcon />} />
     <Menu.Item to="/tool-statuses" primaryText="Tool Statuses" leftIcon={<PrecisionManufacturingRoundedIcon />} />
-    <Menu.Item to="/tool-photos" primaryText="Tool Photos" leftIcon={<PhotoCameraRoundedIcon />} />
-    <Menu.Item to="/tool-assignments" primaryText="Tool Assignments" leftIcon={<AssignmentRoundedIcon />} />
-    <Menu.Item to="/detected-tools" primaryText="Detected Tools" leftIcon={<PrecisionManufacturingRoundedIcon />} />
     <Menu.Item to="/models" primaryText="Models" leftIcon={<ApartmentRoundedIcon />} />
     <Menu.Item to="/brands" primaryText="Brands" leftIcon={<CategoryRoundedIcon />} />
+
+    <Divider sx={{ my: 1.5 }} />
+
+    {open && (
+      <Box sx={{ px: 2, pt: 1, pb: 1 }}>
+        <Typography variant="overline" sx={{ fontWeight: 700, letterSpacing: 1 }}>
+          Assignments
+        </Typography>
+      </Box>
+    )}
+    <Menu.Item to="/tool-assignments" primaryText="Tool Assignments" leftIcon={<AssignmentRoundedIcon />} />
+    <Menu.Item to="/detected-tools" primaryText="Detected Tools" leftIcon={<PrecisionManufacturingRoundedIcon />} />
     <Menu.Item to="/action-types" primaryText="Action Types" leftIcon={<AssignmentRoundedIcon />} />
+
+    <Divider sx={{ my: 1.5 }} />
+
+    {open && (
+      <Box sx={{ px: 2, pt: 1, pb: 1 }}>
+        <Typography variant="overline" sx={{ fontWeight: 700, letterSpacing: 1 }}>
+          Photos
+        </Typography>
+      </Box>
+    )}
+    <Menu.Item to="/tool-photos" primaryText="Tool Photos" leftIcon={<PhotoCameraRoundedIcon />} />
     <Menu.Item to="/photo-types" primaryText="Photo Types" leftIcon={<PhotoCameraRoundedIcon />} />
     <Menu.Item to="/photo-sessions" primaryText="Photo Sessions" leftIcon={<PhotoCameraRoundedIcon />} />
     <Menu.Item to="/photos-for-detection" primaryText="Photos For Detection" leftIcon={<PhotoCameraRoundedIcon />} />
+
+    <Divider sx={{ my: 1.5 }} />
+
+    {open && (
+      <Box sx={{ px: 2, pt: 1, pb: 1 }}>
+        <Typography variant="overline" sx={{ fontWeight: 700, letterSpacing: 1 }}>
+          Locations
+        </Typography>
+      </Box>
+    )}
     <Menu.Item to="/locations" primaryText="Locations" leftIcon={<LocationOnRoundedIcon />} />
     <Menu.Item to="/location-types" primaryText="Location Types" leftIcon={<MapRoundedIcon />} />
-    <Menu.Item to="/roles" primaryText="Roles" leftIcon={<PeopleAltRoundedIcon />} />
+
+    <Divider sx={{ my: 1.5 }} />
+
+    {open && (
+      <Box sx={{ px: 2, pt: 1, pb: 1 }}>
+        <Typography variant="overline" sx={{ fontWeight: 700, letterSpacing: 1 }}>
+          Users
+        </Typography>
+      </Box>
+    )}
     <Menu.Item to="/users" primaryText="Users" leftIcon={<PeopleAltRoundedIcon />} />
-  </Menu>
-);
+    <Menu.Item to="/roles" primaryText="Roles" leftIcon={<PeopleAltRoundedIcon />} />
+    </Menu>
+  );
+};
